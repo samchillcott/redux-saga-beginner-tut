2ab448a1da67ab8039d1ab00df9b11c661d8a4d9
Sam Chillcott <sam@secret-source.eu>
2020-12-30 14:11:24 +0000

    add watcher and worker sagas

--- sagas.js
+++ sagas.js
@@ -1,3 +1,16 @@
+import { put, takeEvery } from 'redux-saga/effects'
+
+const delay = (ms) => new Promise(res => setTimeout(res, ms))
+
 export function* helloSaga() {
   console.log('Hello Sagas!')
+}
+
+export function* IncrementAsync() {
+    yield delay(1000)
+    yield put({type: 'INCREMENT'})
+}
+
+export function* watchIncrementAsync() {
+    yield takeEvery('INCREMENT_ASYNC', IncrementAsync)
 }
